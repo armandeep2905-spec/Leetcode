7f4d7cea2470ff323d2d4f6c59e9232131d292e5
armandeep2905-spec <armandeep2905@gmail.com>
2026-06-17 15:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (87.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
+     
     int majorityElement(vector<int>& nums) {
-        map <int , int > mp;
-        for(int i = 0 ; i < nums.size() ; i++){
-            mp[nums[i]]++;
-        }
-        for(auto it : mp ){
-            if(it.second > nums.size() / 2 ){
-               return it.first;
-            }
-        }
-      return 0 ;
+
+    //     map <int , int > mp;  // better approach 
+    //     for(int i = 0 ; i < nums.size() ; i++){
+    //         mp[nums[i]]++;
+    //     }
+    //     for(auto it : mp ){
+    //         if(it.second > nums.size() / 2 ){
+    //            return it.first;
+    //         }
+    //     }
+    //   return 0 ;
+
+
+
+    // optimised approach => moore voting algo
+
+    int el ;
+    int count = 0 ; 
+
+
+    for(int i = 0 ; i < nums.size() ; i++){
+        if(count ==0 ) el = nums[i];
+        if(nums[i] == el) count ++;
+        if(nums[i] != el) count --;
+        if(count == 0) el = nums[i+1];
+    }
+     int elCount = 0 ;
+    for(int i = 0 ; i < nums.size() ; i++){
+        if(nums[i] ==  el) elCount++;
+
+    }
+    if(elCount > (nums.size() / 2)) return el;
+    else return -1;
     }
 };

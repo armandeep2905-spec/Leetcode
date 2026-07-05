f710366801c92f813d41ff4e3aa7f9c0f5b9d72b
armandeep2905-spec <armandeep2905@gmail.com>
2026-07-05 16:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (50.89%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,9 +1,8 @@
-
-
 class Solution {
 public:
+
 // brute force -> fails , if matrix already has -1, look at the constraints ,ask GPT, very high time complexity
- //   void row(int row , vector<vector<int>>&matrix ){
+//   void row(int row , vector<vector<int>>&matrix ){
 //  for(int j = 0 ; j < matrix[0].size() ; j++){
 //     if(matrix[row][j] != 0 && matrix[row][j] != -1) matrix[row][j] = -1;
 //  }
@@ -33,27 +32,77 @@ public:
 
 
 
+
+
 // better solution->
-void setZeroes(vector<vector<int>>& matrix) {
-vector<int>rows(matrix.size() , 0);
-vector<int>cols(matrix[0].size() , 0);
+// void setZeroes(vector<vector<int>>& matrix) {
+// vector<int>rows(matrix.size() , 0);
+// vector<int>cols(matrix[0].size() , 0);
 
 
-for(int i = 0 ; i <rows.size() ; i ++){
-    for(int j = 0 ; j < cols.size() ; j++){
-        if(matrix[i][j] == 0 ) {
-            rows[i] = 1;
-            cols[j] = 1;
-        }
-    }
-}
+// for(int i = 0 ; i <rows.size() ; i ++){
+//     for(int j = 0 ; j < cols.size() ; j++){
+//         if(matrix[i][j] == 0 ) {
+//             rows[i] = 1;
+//             cols[j] = 1;
+//         }
+//     }
+// }
 
-for(int i = 0 ; i <rows.size() ; i ++){
-    for(int j = 0 ; j < cols.size() ; j++){
-     if(rows[i] == 1 || cols[j] ==1) {matrix[i][j] = 0;}
+// for(int i = 0 ; i <rows.size() ; i ++){
+//     for(int j = 0 ; j < cols.size() ; j++){
+//      if(rows[i] == 1 || cols[j] ==1) 
+//      {
+//         matrix[i][j] = 0;
+//      }
  
-    }
-}
+//     }
+// }
+
+
+
+
+
+//Optimal soln -> 
+
+ void setZeroes(vector<vector<int>>& matrix) {
+     int col0 = matrix[0][0];
+         for ( int i = 0 ; i < matrix.size() ; i++){
+            for(int j = 0 ; j < matrix[0].size() ; j ++){
+                if(matrix[i][j] == 0 ){
+                    matrix[i][0] = 0;    
+                    if( j != 0){
+                        matrix[0][j] = 0;
+                    }          
+                    else col0 = 0;
+                      }
+            }
+         }
+
+
+        for(int i =1 ; i < matrix.size() ; i++){
+            for(int j = 1 ; j<matrix[0].size() ; j++){
+             if(matrix[i][j] != 0 ){
+                if(matrix[i][0] == 0 || matrix [0][j] == 0){
+                    matrix[i][j]= 0;
+                }
+             }
+                }
+
+            
+        }
+
+        if(matrix[0][0] == 0){
+            for(int j = 0 ; j < matrix[0].size() ; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0){
+            for(int i = 0 ; i < matrix.size() ; i++){
+                matrix[i][0] = 0;
+            }
+
+        }
 
     }
 };

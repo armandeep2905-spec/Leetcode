0babf8cbf050e1deb28f05b9750229f06c28e976
armandeep2905-spec <armandeep2905@gmail.com>
2026-07-10 22:32:38 +0530

    Time: 1 ms (37.21%) | Memory: 95.1 MB (61.81%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,22 +1,59 @@
 class Solution {
 public:
-   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int size1 = nums1.size(), size2 = nums2.size();
-    int i = 0, j = 0, count = 0;
-    int idx2 = (size1 + size2) / 2;
-    int idx1 = idx2 - 1;
-    int elem1 = 0;
-    bool even = (size1 + size2) % 2 == 0;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int size1 = nums1.size();
+        int size2 = nums2.size(); 
+        int i = 0 ;
+        int j =0 ;
+        int count = 0;
+        int idx2 = (size1 + size2) / 2;
+        int idx1 = idx2 - 1 ;
+        int elem1 = 0,  elem2 = 0;
+        bool found = false;
+        while( i <= size1 - 1 && j <= size2 - 1){
+          
+            if(nums1[i] <= nums2[j]) {
+                 if(count == idx1 ) elem1 = nums1[i];
+                 if(count == idx2) {elem2 = nums1[i]; found = true ; break;}
+                  count++;
+                  i++;
 
-    while (i < size1 || j < size2) {
-        int val;
-        if (i < size1 && (j >= size2 || nums1[i] <= nums2[j])) val = nums1[i++];
-        else val = nums2[j++];
+            }
+            else {
+                if(count == idx1) elem1 = nums2[j];
+                if(count == idx2) {elem2 = nums2[j]; found = true ;break;}
+                count++;
+                j++;
+            }
 
-        if (count == idx1) elem1 = val;
-        if (count == idx2) return even ? (elem1 + val) / 2.0 : val;
-        count++;
+        }
+        if(!found){
+            while(i <= size1 - 1){
+                 if(count == idx1) elem1 = nums1[i];
+                 if(count == idx2){
+                 elem2 = nums1[i];
+                 found = true ;
+                 break;
+                }
+            count++;
+            i++;
+            }
+
+            while(j <= size2 - 1){
+               if(count == idx1) elem1 = nums2[j];
+               if(count == idx2){
+               elem2 = nums2[j];
+               found = true ;
+               break;
+            }
+            count++;
+            j++;
+            }
+        }
+
+        
+
+        if((size1 + size2) % 2 == 0 ) {return (elem1 + elem2) / 2.0; ;}
+        else return elem2;
     }
-    return 0; // unreachable if arrays aren't both empty
-}
 };

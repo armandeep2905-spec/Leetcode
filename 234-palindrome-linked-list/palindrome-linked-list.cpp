6c8acd8604db017b838983f241ff466448ca7e88
armandeep2905-spec <armandeep2905@gmail.com>
2026-07-15 16:40:07 +0530

    Time: 9 ms (15.81%) | Memory: 118.2 MB (53.00%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,20 +10,64 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp = head;
-        while(temp != NULL){
-            st.push(temp->val);
-            temp=temp->next;
+
+   
+   // bool isPalindrome(ListNode* head) {
+        // approach one with using extra space 
+        // stack<int> st;
+        // ListNode* temp = head;
+        // while(temp != NULL){
+        //     st.push(temp->val);
+        //     temp=temp->next;
+        // }
+
+        // temp = head;
+        // while(!st.empty() || temp != NULL){
+        //     if(st.top() != temp->val) return false;
+        //     temp= temp->next;
+        //     st.pop();
+        // }
+        // return true;
+
+
+        // approach 2 -> no extra space used 
+
+         ListNode* reverseList(ListNode* head){
+        ListNode* curr = head ;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+
         }
 
-        temp = head;
-        while(!st.empty() || temp != NULL){
-            if(st.top() != temp->val) return false;
-            temp= temp->next;
-            st.pop();
+        return prev;
+    }
+
+ bool isPalindrome(ListNode* head) {
+    if(head==NULL || head->next ==NULL) return true; // edge case
+        ListNode* slow = head;  ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            fast = fast->next->next ;
+            slow = slow->next;
         }
+        // mid == slow 
+       
+        ListNode* newHead =  reverseList(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second != NULL){
+            if(first->val != second->val) return false;
+            first = first->next;
+            second = second->next;
+        }
+        reverseList(slow->next);
         return true;
+
+
     }
 };

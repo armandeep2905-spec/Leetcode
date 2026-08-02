7c12360a0e90a81f78d9ab56a305961a855b39f5
armandeep2905-spec <armandeep2905@gmail.com>
2026-08-02 09:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (71.89%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -11,25 +11,38 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    vector<int> inorder(TreeNode* root){
-        if(root == NULL) return ans;
-        inorder(root->left);
-        ans.push_back(root->val);
-        inorder(root->right);
-        return ans;
-    }
-    int getMinimumDifference(TreeNode* root) {
+// soln 1 ->
+    // vector<int> ans;
+    // vector<int> inorder(TreeNode* root){
+    //     if(root == NULL) return ans;
+    //     inorder(root->left);
+    //     ans.push_back(root->val);
+    //     inorder(root->right);
+    //     return ans;
+    // }
+    // int getMinimumDifference(TreeNode* root) {
      
    
-        int ans = 0;
-        vector<int> order;
-     // finding the sorted order
-       order = inorder(root); 
-       int minDis = INT_MAX;
-       for(int i = 1 ; i < order.size() ; i++){
-            minDis = min(minDis , order[i] - order[i-1] );
-       }
-    return minDis;
+    //     int ans = 0;
+    //     vector<int> order;
+    //  // finding the sorted order
+    //    order = inorder(root); 
+    //    int minDis = INT_MAX;
+    //    for(int i = 1 ; i < order.size() ; i++){
+    //         minDis = min(minDis , order[i] - order[i-1] );
+    //    }
+    // return minDis;
+
+//soln 2->
+
+    TreeNode* prev = NULL;
+    int ans = INT_MAX;
+    int getMinimumDifference(TreeNode* root) {
+        if(root == NULL) return ans;
+        if(root -> left !=NULL) getMinimumDifference(root->left);
+        if(prev != NULL)  ans = min (ans , root->val - prev->val);
+        prev = root ;
+        if(root -> right !=NULL)  getMinimumDifference(root->right);
+        return ans;
     }
 };

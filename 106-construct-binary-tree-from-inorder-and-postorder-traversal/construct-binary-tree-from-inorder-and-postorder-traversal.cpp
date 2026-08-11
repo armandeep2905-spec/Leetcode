31c96f4c77565c8339f6a09f8ae436a550c31e59
armandeep2905-spec <armandeep2905@gmail.com>
2026-08-11 12:15:18 +0530

    Time: 80 ms (5.55%) | Memory: 222.4 MB (5.42%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,18 +11,19 @@
  */
 class Solution {
 public:
+ // reversed postorder ->   root -> right -> left 
 
     TreeNode* helper(vector<int> inorder ,  vector<int> postorder , int &idx , int start , int end){
         if(start > end) return NULL;
-        TreeNode* newRoot = new TreeNode(postorder[idx]);
+        TreeNode* newRoot = new TreeNode(postorder[idx]); // built root 
         idx++;
         int in_idx = start;
-        while(newRoot->val != inorder[in_idx] && start <= end){
+        while(newRoot->val != inorder[in_idx] && start <= end){ // extra condition for getting inorder index out of bound
             in_idx++;
 
         }
-        newRoot->right = helper(inorder , postorder  , idx , in_idx+1 , end);
-        newRoot->left = helper(inorder , postorder , idx , start , in_idx-1);
+        newRoot->right = helper(inorder , postorder  , idx , in_idx+1 , end); // built right
+        newRoot->left = helper(inorder , postorder , idx , start , in_idx-1); // built left
         return newRoot;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {

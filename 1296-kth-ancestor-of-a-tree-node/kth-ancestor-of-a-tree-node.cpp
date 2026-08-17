f0de4b05d16ca39a7df942bd0edaaa2c6c99f3f2
armandeep2905-spec <armandeep2905@gmail.com>
2026-08-17 18:03:21 +0530

    Time: 47 ms (85.17%) | Memory: 122.5 MB (88.69%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
@@ -1,42 +1,35 @@
 class TreeAncestor {
 public:
-    vector<vector<int>> up;
-    int LOG;
-
+vector <vector <int>> up;
+int log = 0;
     TreeAncestor(int n, vector<int>& parent) {
-        LOG = 0;
-        while ((1 << LOG) <= n)
-            LOG++;
+        while( (1 << log) <= n ) log++;
+        up.assign(n , vector<int>(log , -1));
 
-        up.assign(n, vector<int>(LOG, -1));
-
-        // 2^0 = 1st ancestor
-        for (int i = 0; i < n; i++) {
+        for (int i = 0 ; i < n ; i++){
             up[i][0] = parent[i];
         }
 
-        // Build 2^j ancestors
-        for (int j = 1; j < LOG; j++) {
-            for (int i = 0; i < n; i++) {
-                if (up[i][j - 1] != -1)
-                    up[i][j] = up[up[i][j - 1]][j - 1];
+        for (int j = 1 ; j < log ; j++){
+            for(int i = 0 ; i < n ; i++){
+                if(up[i][j-1] != -1)  up[i][j] = up[up[i][j-1]][j-1];
             }
         }
     }
-
+    
     int getKthAncestor(int node, int k) {
-
-        for (int j = 0; j < LOG; j++) {
-
-            // If jth bit of k is set
-            if (k & (1 << j)) {
-                node = up[node][j];
-
-                if (node == -1)
-                    return -1;
-            }
+        for ( int i = 0 ; i < log ; i++){
+            if(k & (1 << i)){
+                node = up[node][i];
+                if ( node == -1) return -1;
+            } 
         }
-
         return node;
     }
 };
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
